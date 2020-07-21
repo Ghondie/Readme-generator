cb7a2411d40a7504b509b4e2ba5c4a25c58df211
Jared Schau <jaredschau@MacBook-Pro.local>
2020-07-20 23:00:09 -0600

    finilizing code

--- Develop/index.js
+++ Develop/index.js
@@ -1,16 +1,67 @@
 // array of questions for user
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown")
+const path = require("path")
 const questions = [
+    {
+        type: "input",
+        name: "Title",
+        message: "What is the title of this project?",
+    },
+    {
+        type: "input",
+        name: "description ",
+        message: "Brief description of the project."
+    },
+    {
+        type: "input",
+        name: "table",
+        message: "Table of contents of the project."
+    },
+    {
+        type: "input",
+        name: "instalation",
+        message: "Instalation of the project."
+    },
+    {
+        type: "list",
+        name: "license",
+        message: "What kind of license should your project have?",
+        choices: ["MIT", "APACHE", "GPL", "BSDDDDD", "None"]
+    },
+    {
+        type: "input",
+        name: "contributors",
+        message: "Aditional contributors."
+    },
+    {
+        type: "confirm",
+        name: "tests",
+        message: "any test files"
+    },
 
+    {
+        type: "input",
+        name: "questions",
+        message: "What questions did you have?"
+    }
 ];
 
 // function to write README file
 function writeToFile(fileName, data) {
+    return fs.writeFileSync(path.join(process.cwd(), fileName), data)
 }
 
 // function to initialize program
 function init() {
+    inquirer.prompt(questions).then(data => {
+        console.log(data);
 
+        writeToFile("GENERATEDREADME.md", generateMarkdown(data))
+    })
 }
 
 // function call to initialize program
 init();
+
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,6 +1,13 @@
 // function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  return `# 
+  Title:${data.Title}
+  Description:${data.description}
+  Table of contents:${data.table}
+  Required installed files:${data.instalation}
+  Aditional licenses:${data.license}
+  Any test files:${data.tests}
+  Any additional questions:${data.questions}
 
 `;
 }
